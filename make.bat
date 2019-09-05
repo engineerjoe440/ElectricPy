ba3991fc4ef77c803aeb4315190a118551389fbb
Joe Stanley <Joe_Stanley@selinc.com>
2019-09-05 08:35:54 -0700

    removed shortcut, trying nested

--- make.bat
+++ make.bat
@@ -7,8 +7,8 @@ REM Command file for Sphinx documentation
 if "%SPHINXBUILD%" == "" (
 	set SPHINXBUILD=sphinx-build
 )
-set SOURCEDIR=.
-set BUILDDIR=_build
+set SOURCEDIR=sources
+set BUILDDIR=docs
 
 if "%1" == "" goto help
 
