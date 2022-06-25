78df89ec981ae17ff51bb14eadca31dcc8b2e565
lakshmikanth ayyadevara <lakshmikanth2001@gmail.com>
2022-06-25 11:24:09 +0530

    `exec` ommition in `test`

--- test/test_circle.py
+++ test/test_circle.py
@@ -1,10 +1,10 @@
+import cmath
 from electricpy.geometry.circle import Circle
 from electricpy.geometry import Line, Point
-import cmath
 
-def test_area():
 
-    def test_0():
+class TestArea:
+    def test_0(self):
 
         c = Circle((0, 0), 1)
         assert c.area() == cmath.pi
@@ -12,7 +12,7 @@ def test_area():
         c = Circle((0, 0), 2)
         assert c.area() == cmath.pi * 4
 
-    def test_1():
+    def test_1(self):
 
         c = Circle((0, 0), 1.1)
         assert c.area() == cmath.pi * 1.1**2
@@ -20,12 +20,9 @@ def test_area():
         c = Circle((0, 0), 2.2)
         assert c.area() == cmath.pi * 2.2**2
 
-    for i in range(2):
-        exec("test_{}()".format(i))
 
-def test_circumference():
-
-    def test_0():
+class TestCircumference:
+    def test_0(self):
 
         c = Circle((0, 0), 1)
         assert c.circumference() == cmath.pi * 2
@@ -33,7 +30,7 @@ def test_circumference():
         c = Circle((0, 0), 2)
         assert c.circumference() == cmath.pi * 4
 
-    def test_1():
+    def test_1(self):
 
         c = Circle((0, 0), 1.1)
         assert c.circumference() == cmath.pi * 2.2
@@ -41,12 +38,9 @@ def test_circumference():
         c = Circle((0, 0), 2.2)
         assert c.circumference() == cmath.pi * 4.4
 
-    for i in range(2):
-        exec("test_{}()".format(i))
 
-def test_tangent():
-
-    def test_0():
+class TestTangent:
+    def test_0(self):
         c = Circle((0, 0), 1)
 
         assert c.tangent(Point(0, 1)) == Line(0, 1, -1)
@@ -54,25 +48,21 @@ def test_tangent():
         assert c.tangent(Point(1, 0)) == Line(1, 0, -1)
         assert c.tangent(Point(-1, 0)) == Line(-1, 0, -1)
 
-
-    def test_1():
+    def test_1(self):
 
         from test import compare_lines
 
         c = Circle((0, 0), 1)
 
-        p = Point(cmath.cos(cmath.pi/4), cmath.sin(cmath.pi/4))
+        p = Point(cmath.cos(cmath.pi / 4), cmath.sin(cmath.pi / 4))
         p1 = Point(cmath.sqrt(2), 0)
         p2 = Point(0, cmath.sqrt(2))
 
         assert compare_lines(c.tangent(p), Line.construct(p1, p2))
 
-    for i in range(2):
-        exec("test_{}()".format(i))
 
-def test_normal():
-
-    def test_0():
+class TestNormal:
+    def test_0(self):
         c = Circle((0, 0), 1)
 
         assert c.normal(Point(0, 1)) == Line(1, 0, 0)
@@ -80,16 +70,13 @@ def test_normal():
         assert c.normal(Point(1, 0)) == Line(0, 1, 0)
         assert c.normal(Point(-1, 0)) == Line(0, 1, 0)
 
-    def test_1():
+    def test_1(self):
 
         from test import compare_lines
 
         c = Circle((0, 0), 1)
-        p0 = Point(cmath.cos(cmath.pi/4), cmath.sin(cmath.pi/4))
-        p1 = Point(-cmath.cos(cmath.pi/4), cmath.sin(cmath.pi/4))
+        p0 = Point(cmath.cos(cmath.pi / 4), cmath.sin(cmath.pi / 4))
+        p1 = Point(-cmath.cos(cmath.pi / 4), cmath.sin(cmath.pi / 4))
 
         assert compare_lines(c.normal(p0), Line(1, -1, 0))
         assert compare_lines(c.normal(p1), Line(1, 1, 0))
-
-    for i in range(2):
-        exec("test_{}()".format(i))
