b6ad142f8d419bf275c043a8ab0cf4028e78143d
lakshmikanth ayyadevara <lakshmikanth2001@gmail.com>
2022-07-03 19:30:48 +0530

    workflow corrections

--- electricpy/version.py
+++ electricpy/version.py
@@ -1,2 +1,11 @@
+################################################################################
+"""
+`electricpy` Package - `version` Module.
+
+>>> from electricpy import version
+
+This file is to keep track of Name, Current Version of electricpy for CI and CD
+"""
+################################################################################
 NAME = "electricpy"
 VERSION = "0.2.2"
