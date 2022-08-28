233277633a28fac6f622071a11390d21e12ba27c
Joe Stanley <engineerjoe440@yahoo.com>
2022-08-28 09:39:00 -0700

    bumped version

--- electricpy/version.py
+++ electricpy/version.py
@@ -8,4 +8,4 @@ This file is to keep track of Name, Current Version of electricpy for CI and CD
 """
 ################################################################################
 NAME = "electricpy"
-VERSION = "0.2.2"
+VERSION = "0.3.0"
